3ad884d978325b83fa08ab9ba059736bc9f01dd1
viny27 <vinicius.ferneda@gmail.com>
2014-04-19 16:58:48 -0300

    Ajustada consulta de conjunto evidencias

--- certics/src/main/java/vinicius/ferneda/tcc/certics/view/ConjuntoEvidenciasListMB.java
+++ certics/src/main/java/vinicius/ferneda/tcc/certics/view/ConjuntoEvidenciasListMB.java
@@ -1,45 +1,29 @@
 package vinicius.ferneda.tcc.certics.view;
 
-import java.util.Iterator;
 import java.util.List;
 
 import javax.inject.Inject;
 
-import vinicius.ferneda.tcc.certics.business.ConjuntoEvidenciasBC;
-import vinicius.ferneda.tcc.certics.domain.ConjuntoEvidenciasEntity;
+import vinicius.ferneda.tcc.certics.business.AvaliacaoBC;
+import vinicius.ferneda.tcc.certics.domain.AvaliacaoEntity;
 import br.gov.frameworkdemoiselle.annotation.NextView;
 import br.gov.frameworkdemoiselle.annotation.PreviousView;
 import br.gov.frameworkdemoiselle.stereotype.ViewController;
 import br.gov.frameworkdemoiselle.template.AbstractListPageBean;
-import br.gov.frameworkdemoiselle.transaction.Transactional;
 
 @ViewController
 @NextView("./conjuntoEvidencias_edit.jsf")
 @PreviousView("./conjuntoEvidencias_list.jsf")
-public class ConjuntoEvidenciasListMB extends AbstractListPageBean<ConjuntoEvidenciasEntity, Long> {
+public class ConjuntoEvidenciasListMB extends AbstractListPageBean<AvaliacaoEntity, Long> {
 
 	private static final long serialVersionUID = 1L;
 
 	@Inject
-	private ConjuntoEvidenciasBC conjuntoEvidenciasBC;
+	private AvaliacaoBC avaliacaoBC;
 	
 	@Override
-	protected List<ConjuntoEvidenciasEntity> handleResultList() {
-		return this.conjuntoEvidenciasBC.findAll();
-	}
-	
-	@Transactional
-	public String deleteSelection() {
-		boolean delete;
-		for (Iterator<Long> iter = getSelection().keySet().iterator(); iter.hasNext();) {
-			Long id = iter.next();
-			delete = getSelection().get(id);
-			if (delete) {
-				conjuntoEvidenciasBC.delete(id);
-				iter.remove();
-			}
-		}
-		return getPreviousView();
+	protected List<AvaliacaoEntity> handleResultList() {
+		return this.avaliacaoBC.findAll();
 	}
 
 }
